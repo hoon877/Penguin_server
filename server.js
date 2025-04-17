239ebc65f5dbdc50f6aa66dbaff741a344a74041
hoon877 <simhoon0905@naver.com>
2025-04-17 18:07:31 +0900

    04.17 작업내용

--- server.js
+++ server.js
@@ -3,38 +3,86 @@ import { v4 as uuidv4 } from "uuid";
 
 const players = {};
 const socketRooms = new Map();
+const roomList = [];
 const TICK_RATE = 20; // 초당 20번
 const TICK_INTERVAL = 1000 / TICK_RATE;
 const io = new Server(3000);
+const activeRooms = new Set();
+var rooms = [];
+var Users = [];
 
 io.on("connection", (socket) => {
-    console.log("a user connected");
+    Users[socket.id] = {
+        id: socket.id,
+        nickname: "",
+        Room: "",
+      }
 
-    socket.on('createRoom', function() {
-        const roomId = uuidv4(); // 서버가 생성
-        socket.join(roomId);
-        socket.emit('createdRoom', { roomId });
-        socketRooms.set(socket.id, roomId);
-        players[socket.id] = { x: 0, y: 0, dirX: 0, dirY: 0, speed: 2, isAlive: true };
-    });
-
-    socket.on('joinRoom', function({ roomId }) {
-        const room = io.sockets.adapter.rooms.get(roomId);
+      socket.on('LoginCheck', name => {
+        //접속하기 버튼을 누르면 입장합니다.
     
-        if (room) {
-            socket.join(roomId);
-            socket.emit('joinedRoom', { roomId });
-            socketRooms.set(socket.id, roomId);
-            players[socket.id] = { x: 0, y: 0, dirX: 0, dirY: 0, speed: 2 };
-        } else {
-            socket.emit('errorJoin', { message: "존재하지 않는 방입니다." });
+        var check = true;
+        //변수 하나를 생성
+    
+        for (var k in Users) {
+          if (Users[k].nickname == name) {
+            check = false;
+            break;
+          }
+        }
+        //닉네임이 있는지 없는지 파악합니다.
+    
+        if (check) {
+          //닉네임이 없다면 생성
+          Users[socket.id].nickname = name
+          //nickname 설정
+          console.log(name + ": 로비진입 성공!")
+          socket.emit('Login')
         }
     });
 
-    socket.on('leaveRoom', function(roomData) {
+    console.log("a user connected");
+
+    socket.on("getRoomList", () => {
+        console.log("getRoomList event received");
+        socket.emit("roomList", {rooms : roomList});
+    });
+
+    socket.on("createRoom", function() {
+        console.log("createRoom event received");
+        const roomId = uuidv4(); // 서버가 생성
+        socket.join(roomId);
+        socket.emit("createdRoom", { roomId });
+        socketRooms.set(socket.id, roomId);
+        players[socket.id] = { x: 0, y: 0, dirX: 0, dirY: 0, speed: 2, isAlive: true };
+        roomList.push(roomId); // 방 리스트에 추가
+        activeRooms.add(roomId);
+        console.log('방 생성됨: ' + roomId);
+    });
+
+    socket.on("joinRoom", function({ roomId }) {
+        if (!roomId) {
+            socket.emit("errorJoin", { message: "잘못된 방 ID입니다." });
+            return;
+        }
+
+        const room = io.sockets.adapter.rooms.get(roomId);
+        console.log("joinRoom event received", roomId, room);
+
+        if (room) {
+            socket.join(roomId);
+            socket.emit("joinedRoom", { roomId });
+            socketRooms.set(socket.id, roomId);
+            players[socket.id] = { x: 0, y: 0, dirX: 0, dirY: 0, speed: 2 };
+        } else {
+            socket.emit("errorJoin", { message: "존재하지 않는 방입니다." });
+        }
+    });
+
+    socket.on("leaveRoom", function(roomData) {
         socket.leave(roomData.roomId);
-        socket.emit('exitRoom');
-        socket.to(roomData.roomId).emit('endGame');
+        socket.emit("exitRoom");
+        socket.to(roomData.roomId).emit("endGame");
 
         var roomId = socketRooms.get(socket.id);
         const roomIdx = rooms.indexOf(roomId);
@@ -45,7 +93,7 @@ io.on("connection", (socket) => {
         socketRooms.delete(socket.id);
     });
 
-    socket.on('move', (dir) => {
+    socket.on("move", (dir) => {
         // 방향 정보만 저장 (dir = { x: -1, y: 0 } 등)
         if (players[socket.id]) {  // players 객체가 존재하는 경우에만 처리
             players[socket.id].dirX = dir.x;
@@ -53,33 +101,34 @@ io.on("connection", (socket) => {
         }
     });
 
-    socket.on('gameOver', async function(data) {
+    socket.on("gameOver", async function(data) {
         const { roomId, winner, myPlayerType } = data;
         console.log(`게임 종료: Room(${roomId}), 승자: ${winner}, 내 플레이어 타입: ${myPlayerType}`);
 
-        socket.to(roomId).emit('gameEnded', { winner: winner });
+        socket.to(roomId).emit("gameEnded", { winner: winner });
     });
 
-    socket.on('sendMessage', function(message) {
+    socket.on("sendMessage", function(message) {
         console.log('메시지를 받았습니다: ' + message.roomId + ' ' + message.nickName + ' : ' + message.message);
         socket.to(message.roomId).emit('receiveMessage', { nickName: message.nickName, message: message.message });
     });
 
-    socket.on('disconnect', () => {
+    socket.on("disconnect", () => {
         const roomId = socketRooms.get(socket.id);
         if (roomId) {
             socketRooms.delete(socket.id);  // socketRooms에서 해당 소켓 제거
         }
         delete players[socket.id]; // 플레이어 데이터 삭제
+        delete Users[socket.id]; // 사용자 데이터 삭제
     });
 
-    socket.on('reconnectToRoom', (data) => {
+    socket.on("reconnectToRoom", (data) => {
         const { roomId } = data;
         socket.join(roomId);
-        socket.emit('reconnected', { roomId });
+        socket.emit("reconnected", { roomId });
     });
 
-    socket.on('kill', (data) => {
+    socket.on("kill", (data) => {
         const { targetId } = data;
         const roomId = socketRooms.get(socket.id);
     
@@ -93,10 +142,10 @@ io.on("connection", (socket) => {
     
         players[targetId].isAlive = false;
     
-        io.to(roomId).emit('killed', { victimId: targetId, killerId: socket.id });
+        io.to(roomId).emit("killed", { victimId: targetId, killerId: socket.id });
     });
 
-    socket.on('fishing', (data) => {
+    socket.on("fishing", (data) => {
         // TODO: fishing 이벤트 처리 로직 구현
     });
 });
@@ -123,7 +172,7 @@ setInterval(() => {
 
     // 각 방에만 해당 상태 전송
     for (const roomId in roomPlayerMap) {
-        io.to(roomId).emit('updatePlayers', roomPlayerMap[roomId]);
+        io.to(roomId).emit("updatePlayers", roomPlayerMap[roomId]);
     }
 }, TICK_INTERVAL);
 
